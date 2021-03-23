a840589e6c5cd04123381851568e2a936b6de72a
Ariq2901 <ariqkage@gmail.com>
2021-03-23 12:37:44 +0700

    setting up MongoDB Atlas connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,20 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express()
 const PORT = 8001
+
+const {
+  MONGODB_ATLAS_USERNAME,
+  MONGODB_ATLAS_PASSWORD,
+  MONGODB_ATLAS_DBNAME
+} = process.env
+
+const uri = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.uorii.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello From Express')
@@ -11,6 +24,12 @@ app.get('/about', (req: Request, res: Response) => {
   res.send('This is about route')
 })
 
-app.listen(PORT, () => {
-  console.info(`App is listening on port ${PORT}`)
-})
+mongoose.set('useFindAndModify', true)
+mongoose.connect(uri, options)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.info(`App is listening on port ${PORT}`)
+    })
+  }).catch((error) => {
+    throw error
+  })
