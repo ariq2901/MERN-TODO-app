e68bd833895b927ed4af2516638f21e2e298dd33
Ariq2901 <ariqkage@gmail.com>
2021-03-24 22:11:54 +0700

    setting up tailwindcss configuration & add additional assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container relative bg-darkPurple mx-auto max-w-lg p-4 box-border min-h-screen">
+      
+    </main>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import webFontLoader from 'webfontloader';
+
+import 'assets/styles/main.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+webFontLoader.load({
+  google: {
+    families: ['Raleway:400,700:latin', 'Montserrat:700:latin']
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
